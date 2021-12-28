c28ca9748589fb036f89dd9634fccb02b654df0e
Ntinos <konstantinides.kon@gmail.com>
2021-12-28 13:40:12 +0200

    Update Josephus.c

--- Josephus.c
+++ Josephus.c
@@ -1,5 +1,5 @@
 /*************************************************************************
-        Copyright © 2021 Konstantinidis Konstantinos
+        Copyright Â© 2021 Konstantinidis Konstantinos
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 *************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
 typedef int QueueElementType;
