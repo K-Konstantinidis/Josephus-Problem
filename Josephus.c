a8ab76e131f65b61f5b71e116177fddba120c5f3
Ntinos <konstantinides.kon@gmail.com>
2021-12-28 13:54:20 +0200

    Update Josephus.c

--- Josephus.c
+++ Josephus.c
@@ -21,8 +21,8 @@ typedef int QueueElementType;
 typedef struct QueueNode *QueuePointer;
 
 typedef struct QueueNode{
-	QueueElementType Data;
-    QueuePointer Next;
+    QueueElementType Data; //Data of the node
+    QueuePointer Next; //Pointer that shows the next node
 } QueueNode;
 
 typedef struct{
@@ -44,13 +44,13 @@ main(){
     QueuePointer CurPtr, PredPtr;
     int number, no_of_execution, j=1;
 
-    CreateQ(&Queue);
+    CreateQ(&Queue); //Create the queue
 
     printf("Insert the number of the prisoners: ");
     scanf("%d", &number);
 
     for(i=1; i<=number; i++)
-        AddQ(&Queue, i);
+        AddQ(&Queue, i); //Add the prisoners in the queue
 
     printf("Insert the number of the execution cycle: ");
     scanf("%d", &no_of_execution);
@@ -59,20 +59,20 @@ main(){
 
     while(j < number){
         for(i=1; i<=no_of_execution; i++){
-            printf("%d, ", CurPtr->Data);
+            printf("%d, ", CurPtr->Data); //Show the prisoner number
             if(i!=no_of_execution){
                 PredPtr = CurPtr;
-                CurPtr = CurPtr->Next;
+                CurPtr = CurPtr->Next; //Change the current pointer to the next one
             }
         }
-        PredPtr->Next = CurPtr->Next;
-        printf("EXECUTED: %d\n", CurPtr->Data);
-        free(CurPtr);
-        CurPtr = PredPtr->Next;
+        PredPtr->Next = CurPtr->Next; //Give the value of the next pointer to pred pointer next
+        printf("EXECUTED: %d\n", CurPtr->Data); //Show the current pointer data
+        free(CurPtr); //Free the current pointer
+        CurPtr = PredPtr->Next; //Give back the value of the pred next pointer to the current one
 
         j++;
     }
-    printf("SURVIVED: %d\n", CurPtr->Data);
+    printf("SURVIVED: %d\n", CurPtr->Data); //Show the survivor
 
     system("PAUSE");
 }
@@ -87,12 +87,12 @@ boolean EmptyQ(QueueType Queue){
 }
 
 void AddQ(QueueType *Queue, QueueElementType Item){
-	QueuePointer TempPtr;
+    QueuePointer TempPtr;
 
     TempPtr = (QueuePointer)malloc(sizeof(struct QueueNode));
     TempPtr->Data = Item;
     TempPtr->Next = NULL;
-    if(Queue->Front==NULL)
+    if(Queue->Front==NULL) //If queue is empty
         Queue->Front=TempPtr;
     else
         Queue->Rear->Next = TempPtr;
